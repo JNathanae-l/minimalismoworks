e1ba5a0381554eb3e4b27e1b2ddd7efe4137a737
nathanael <natha_ml@icloud.com>
2023-07-24 10:31:38 -0600

    modification to letters of vision, mission, and principles

--- js/logic.js
+++ js/logic.js
@@ -1,33 +1,52 @@
-var flag = true;
-var textEmpty = "";
-function remplazarVision() {
-    var textVision = document.getElementById("text-h4-vision");
-    if (flag == true) {
-        textVision.style.color="white";
-    } else{
-        textVision.style.color="black";
 
-    }
-    flag = !flag;
-    return flag;
+
+function hiddeComponentsVision() {
+
+    var componentsVision = document.getElementById("content-principal-vision");
+    var componentsVisionText = document.getElementById("allVision");
+    componentsVision.style.display='none'
+    componentsVisionText.style.display='flex'
 }
-function remplazarMision() {
-    var textMision = document.getElementById("text-h4-mision");
-    if (flag == true) {
-        textMision.style.color="white";
-    } else{
-        textMision.style.color="black";
-    }
-    flag = !flag;
-    return flag;
+
+function showComponentsVision() {
+
+    var componentsVision = document.getElementById("content-principal-vision");
+    componentsVision.style.display='flex';
+    var componentsVisionText = document.getElementById("allVision");
+    componentsVisionText.style.display='none'
 }
-function remplazarPrinciples() {
-    var textPrinciples = document.getElementById("text-h4-principles");
-    if (flag == true) {
-        textPrinciples.style.color="white";
-    } else{
-        textPrinciples.style.color="black";
-    }
-    flag = !flag;
-    return flag;
+
+function hiddeComponentsMision() {
+
+    var componentsMision = document.getElementById("content-principal-mision");
+    var componentsMisionText = document.getElementById("allMision");
+    componentsMision.style.display='none'
+    componentsMisionText.style.display='flex'
 }
+
+function showComponentsMision() {
+
+    var componentsMision = document.getElementById("content-principal-mision");
+    componentsMision.style.display='flex';
+    var componentsMisionText = document.getElementById("allMision");
+    componentsMisionText.style.display='none'
+}
+
+function hiddeComponentsPrinciples() {
+
+    var componentsPrinciples = document.getElementById("content-principal-principles");
+    var componentsPrinciplesText = document.getElementById("allPrinciples");
+    componentsPrinciples.style.display='none'
+    componentsPrinciplesText.style.display='flex'
+}
+
+function showComponentsPrinciples() {
+
+    var componentsPrinciples = document.getElementById("content-principal-principles");
+    componentsPrinciples.style.display='flex';
+    var componentsPrinciplesText = document.getElementById("allPrinciples");
+    componentsPrinciplesText.style.display='none'
+}
+
+
+
